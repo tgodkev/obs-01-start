8f3a10fc17ea9cbe9cdfc519c657964769fa9ad9
kevin knight <tgodkev@gmail.com>
2022-05-17 14:32:04 -0400

    test commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,12 +28,29 @@ export class HomeComponent implements OnInit, OnDestroy {
       setInterval(() => {
         // next emits a new value.
         observer.next(count);
+        // complete stops observables when they meet requirements.
+        if (count === 2) {
+          observer.complete();
+        }
+        // error cancels the observable does NOT complete
+        if (count > 3) {
+          observer.error(new Error('count is greater than 3!'))
+        }
         count++
       }, 1000)
     });
     // subscribing to custom observable, to subscribe pass a function that accepts emitted data
+    // emiting data is the most common use case. 80% of the time first argument will be data your getting.
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data)
+      // second function is for errors and takes the error as an argument.
+    }, error => {
+      console.log(error)
+      alert(error.message);
+      // third function is to react to completion of the observable
+      // Do not need to unsubscribed if observable is completed
+    }, () => {
+      console.log('Completed.')
     })
 
   }
